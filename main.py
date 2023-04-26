a91d910370b0a3e6a507b7a58f1aa93b694d63d6
GHerschberger <91208486+GHerschberger@users.noreply.github.com>
2023-04-26 16:33:13 +0000

    Added HTML form, added new python function to read input

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-from flask import Flask
+from flask import Flask, request
 from caesar import rotate_string
 
 app = Flask('app')
 
-@app.route('/')
-def hello_world():
-  return '<h1>hello</h1>  <p>this is a paragraph</p>'
+form = """
+<!DOCTYPE html>
 
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form method="post">
+      <input type="text" name="rot" value="0">
+        <textarea name="text"> </textarea>
+    <input type="submit" value="Submit Query">
+        </form>
+    </body>
+</html>
+
+""" 
+
+
+@app.route('/')
+def index():
+  return form
+
+@app.route('/', methods=['POST'])
+def encrypt():
+  rot = int(request.form['rot'])
+  text = request.form['text']
+  
 
 
 app.run(host='0.0.0.0', port=8080)
